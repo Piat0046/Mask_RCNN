bb596648087473797713a4c37831ee53ba84da11
ji hun park <piat@jiui-MacBookAir.local>
2022-05-09 18:11:12 +0900

    test1

--- samples/balloon/dataset/train/covert.py
+++ samples/balloon/dataset/train/covert.py
@@ -8,14 +8,13 @@ with open(path, "r") as st_json:
 
     st_python = json.load(st_json)
 file_list = list(st_python.keys())
-pprint(file_list[0]+str(st_python[file_list[0]]['size']))
 
 doc = {}
 for i in range(len(file_list)):
     region = {}
     for a in range(len(st_python[file_list[i]]['regions'])):
         region[a] = {'shape_attributes' : st_python[file_list[i]]['regions'][a]['shape_attributes'], 'region_attributes' : st_python[file_list[i]]['regions'][a]['region_attributes']}
-    doc[file_list[i]+str(st_python[file_list[i]]['size'])] = {"fileref": "", 'size': st_python[file_list[i]]['size'], 'filename':file_list[i],'base64_img_data':"", 'file_attributes': {}, 'regions' : region}
+    doc[file_list[i]] = {"fileref": "", 'size': st_python[file_list[i]]['size'], 'filename':st_python[file_list[i]]['filename'],'base64_img_data':"", 'file_attributes': {}, 'regions' : region}
 
 with open('via_region_data.json', 'w', encoding='UTF-8') as f:
     json.dump(doc, f)
